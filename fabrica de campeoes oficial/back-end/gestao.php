34b6f1c330e2ad2b8ed0c5f04a710aa93f4ae640
Builtter <dego.dvl@gmail.com>
2025-05-24 02:31:58 -0300

    Recuperando dados de estoque para tela de gestão

--- fabrica de campeoes oficial/back-end/gestao.php
+++ fabrica de campeoes oficial/back-end/gestao.php
@@ -3,29 +3,35 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "fabricadecampeoes";
-$port = '3307';
+$port = '3306';
 
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
 if ($conn->connect_error) {
     die("Erro na conexão: " . $conn->connect_error);
 }
 
+//Dashboard data
+
+//Resgata pedidos cadastrados
 $result = $conn->query("SELECT COUNT(*) as total FROM pedido");
 $totalPedidos = $result->fetch_assoc();
 
+//Resgata qtd clientes cadastrados
 $result = $conn->query("SELECT COUNT(*) as total FROM cliente");
 $totalClientes = $result->fetch_assoc();
 
+//Resgata valor total → total investido em produtos ou vendidos?
 // $result = $conn->query("SELECT SUM(valor) as total FROM pedidos");
 // $faturamentoTotal = $result->fetch_assoc();
 
-// $result = $conn->query("SELECT COUNT(*) as total FROM estoque");
-// $produtosEmEstoque = $result->fetch_assoc();
+//Resgata qtd produtos cadastrados
+$result = $conn->query("SELECT SUM(quantidade) as total FROM produto");
+$produtosEmEstoque = $result->fetch_assoc();
 
 echo json_encode(array(
     'totalPedidos' => $totalPedidos['total'],
     'totalClientes' => $totalClientes['total'],
     // 'faturamentoTotal' => $faturamentoTotal['total'],
-    // 'produtosEmEstoque' => $produtosEmEstoque['total']
+    'produtosEmEstoque' => $produtosEmEstoque['total']
 ));
 
